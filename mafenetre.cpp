78e48b34db8178621129561cc09536a8c467bd5b
mfd <mfdewulf@gmail.com>
2018-09-25 11:37:33 +0200

    Modification de mafenetre.cpp mafenetre.h pour implementation QSlider et QLCDNumber connectés

--- mafenetre.cpp
+++ mafenetre.cpp
@@ -3,31 +3,34 @@
  MaFenetre::MaFenetre() : QWidget()
 
 {
-    setFixedSize(300, 150);
+    setFixedSize(200, 100);
 
-    // Construction du bouton
-    m_bouton = new QPushButton("Pimp mon bouton !", this);
+    m_lcd = new QLCDNumber(this);
+    m_lcd->setSegmentStyle(QLCDNumber::Flat);
+    m_lcd->move(50, 20);
 
-    m_bouton->setFont(QFont("Comic Sans MS", 14));
-    m_bouton->setCursor(Qt::PointingHandCursor);
-    m_bouton->setIcon(QIcon("smile.jpg"));
-    m_bouton->move(60, 50);
+    m_slider = new QSlider(Qt::Horizontal, this);
+    m_slider->setGeometry(10,60,150,20);
+
+    QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_lcd, SLOT(display(int))) ;
 }
 
  MaFenetre::MaFenetre(int p_largeur, int p_hauteur) : QWidget()
  {
      setFixedSize(p_largeur, p_hauteur);
 
-     // Construction du bouton
-     m_bouton = new QPushButton("Pimp mon bouton !", this);
+     m_lcd = new QLCDNumber(this);
+     m_lcd->setSegmentStyle(QLCDNumber::Flat);
+     m_lcd->move(50, 20);
 
-     m_bouton->setFont(QFont("Comic Sans MS", 14));
-     m_bouton->setCursor(Qt::PointingHandCursor);
-     m_bouton->setIcon(QIcon("smile.jpg"));
-     m_bouton->move(60, 50);
+     m_slider = new QSlider(Qt::Horizontal, this);
+     m_slider->setGeometry(10,60,150,20);
+
+     QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_lcd, SLOT(display(int))) ;
  }
 
 MaFenetre::~MaFenetre()
 {
-    delete m_bouton;
+    delete m_lcd;
+    delete m_slider;
 }
